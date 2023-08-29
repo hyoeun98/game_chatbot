a982e7ced253c60c453f92a20de3b8914c9d7656
hyoeun98 <f2921641@naver.com>
2023-08-30 02:46:31 +0900

    edit api call

--- crawling/main.py
+++ crawling/main.py
@@ -1,13 +1,14 @@
 import requests, json, os
 from loading import printProgressBar
 import concurrent.futures
-
+import time
 FOLDER_PATH = "output"
 
 if not os.path.exists(FOLDER_PATH):
 	os.makedirs(FOLDER_PATH)
 
 def call_api(idx):
+    
 	response = requests.get(f"https://steamspy.com/api.php?request=all&page={idx}")
 
 	data = response.json()
@@ -16,9 +17,10 @@ def call_api(idx):
 	l = len(data)
 
 	print('[Total data]:', l)
-	# printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+	printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 	for game_id, obj in data.items():
+		time.sleep(0.5)
 		game_name = obj["name"]
 		try:
 			res = requests.get(f"https://store.steampowered.com/api/appdetails?appids={game_id}")
@@ -30,22 +32,19 @@ def call_api(idx):
 				json.dump(detail, f, indent=4)
 		except TypeError:
 			print(res)
-			...
-		except:
-			print(res)
-		# printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+		printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 		i+= 1
 
-	return 1
 	
-def multi_threading_call_api(page_num):
-    result = 0
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as exe:
-        future_to_api = {exe.submit(call_api, idx): idx for idx in range(page_num)}
-        for future in concurrent.futures.as_completed(future_to_api):
-            result += (future.result())
+# def multi_threading_call_api(page_num):
+#     result = 0
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as exe:
+#         future_to_api = {exe.submit(call_api, idx): idx for idx in range(page_num)}
+#         for future in concurrent.futures.as_completed(future_to_api):
+#             result += (future.result())
 
-
-    print(result)
+    # print(result)
 # multi_threading_call_api(5)
-call_api(1)
+for i in range(5, 65):
+	call_api(i)
